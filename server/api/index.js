02200c7d503deeabe713cfd3008b66f97eb4669d
cofuente <contact@cofuente.io>
2020-03-08 16:34:41 -0400

    added forms route path

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/admins', require('./admins'))
+router.use('/forms', require('./forms'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
