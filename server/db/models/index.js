ed8a7f151e081cd8563790959d9938d5eaed2c80
cofuente <contact@cofuente.io>
2020-03-08 16:10:45 -0400

    clarified comment

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,12 +4,7 @@ const Application = require('./Application')
 const Comment = require('./Comment')
 const Form = require('./Form')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(Admin)
- */
+/* ----- Associations Go Here ----- */
 Application.belongsTo(Applicant)
 Form.hasOne(Applicant)
 Comment.belongsTo(Admin)
@@ -18,7 +13,7 @@ Application.hasMany(Comment)
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
  * for example, we can say: const {Admin} = require('../db/models')
- * instead of: const Admin = require('../db/models/user')
+ * instead of: const Admin = require('../db/models/admin')
  */
 module.exports = {
   Admin,
