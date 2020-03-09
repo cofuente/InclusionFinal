ea3d59164c8b8714b49499304a45d32e16343514
Vanessa Chan <vchan8084@gmail.com>
2020-03-08 20:07:18 -0400

    Created route to create a new applicant

--- server/api/applicants/index.js
+++ server/api/applicants/index.js
@@ -4,10 +4,17 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const applicants = await Applicant.findAll({
-      attributes: ['name']
-    })
+    const applicants = await Applicant.findAll()
     res.json(applicants)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newApplicant = await Applicant.create(req.body)
+    res.json(newApplicant)
   } catch (err) {
     next(err)
   }
