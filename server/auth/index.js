1d703e1a3da2c4e7f25af135f2c26c90ba30b90a
cofuente <contact@cofuente.io>
2020-03-14 18:33:55 -0400

    altered auth app to use Admin rather than User

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,18 +1,18 @@
 const router = require('express').Router()
-const User = require('../db/models')
+const {Admin} = require('../db/models')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
   try {
-    const user = await User.findOne({where: {email: req.body.email}})
-    if (!user) {
-      console.log('No such user found:', req.body.email)
+    const admin = await Admin.findOne({where: {name: req.body.name}})
+    if (!admin) {
+      console.log('No such admin found:', req.body.name)
       res.status(401).send('Wrong username and/or password')
-    } else if (!user.correctPassword(req.body.password)) {
-      console.log('Incorrect password for user:', req.body.email)
+    } else if (!admin.correctPassword(req.body.password)) {
+      console.log('Incorrect password for admin:', req.body.name)
       res.status(401).send('Wrong username and/or password')
     } else {
-      req.login(user, err => (err ? next(err) : res.json(user)))
+      req.login(admin, err => (err ? next(err) : res.json(admin)))
     }
   } catch (err) {
     next(err)
@@ -21,11 +21,11 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
-    req.login(user, err => (err ? next(err) : res.json(user)))
+    const admin = await Admin.create(req.body)
+    req.login(admin, err => (err ? next(err) : res.json(admin)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
-      res.status(401).send('User already exists')
+      res.status(401).send('Admin already exists')
     } else {
       next(err)
     }
@@ -39,7 +39,7 @@ router.post('/logout', (req, res) => {
 })
 
 router.get('/me', (req, res) => {
-  res.json(req.user)
+  res.json(req.admin)
 })
 
 router.use('/google', require('./google'))
