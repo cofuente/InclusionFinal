137b72992baf6bc9e6328001cb5f84f8e6f764d1
cofuente <contact@cofuente.io>
2020-03-15 03:21:20 -0400

    deleted signup

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Landing, Fallback} from './components'
+import {Login, UserHome, Landing, Fallback} from './components'
 import {me} from './store'
 
 /**
