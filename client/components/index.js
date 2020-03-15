50ffc0dfbed055666c6ec76571c9c67fcb43fe4d
cofuente <contact@cofuente.io>
2020-03-15 02:29:11 -0400

    added Landing component

--- client/components/index.js
+++ client/components/index.js
@@ -4,5 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
+export {default as Landing} from './landing'
 export {default as UserHome} from './user-home'
 export {Login} from './auth-form'
---
+++ client/components/landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * COMPONENT
+ */
+const Landing = () => {
+  return (
+    <div>
+      <h1>THIS IS THE LANDING PAGE. YAY! </h1>
+    </div>
+  )
+}
+
+export default Landing
