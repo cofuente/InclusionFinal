3e32a417c5a7f2343c0569569626ec949f7d5538
cofuente <contact@cofuente.io>
2020-03-15 03:19:10 -0400

    added landing page path

--- client/routes.js
+++ client/routes.js
@@ -19,6 +19,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={Landing} />
         <Route path="/login" component={Login} />
         {isLoggedIn && (
           <Switch>
